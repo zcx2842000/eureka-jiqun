b8fb64992becba9cacf82c77b287e7a58f917f33
luoshudong <luoshudong@lsd>
2019-12-28 14:14:27 +0800

    eureka 集群，作用：负载均衡以及数据同步（AP）

---
+++ eureka-server/src/main/java/com/example/eurekaserver/controller/QuetyController.java
@@ -0,0 +1,19 @@
+package com.example.eurekaserver.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/query")
+public class QuetyController {
+    @Autowired
+    EurekaClientConfigBean eurekaClientConfigBean;
+    @GetMapping("eureka-server")
+    public Object getEurekaServerUrl(){
+        return eurekaClientConfigBean.getServiceUrl();
+    }
+}
